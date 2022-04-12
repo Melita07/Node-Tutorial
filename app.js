5523f013d829e1da6d877f910a1fab4f09be4714
Melita Pereira <melitapereira07@gmail.com>
2022-04-12 18:39:16 -0400

    InbuiltModule-OS

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-//CommonJS, every file is module (by default)
-//Modules - encapsulated code (only share minimum)
+const path = require('path')
 
+console.log(path.sep)
 
-//when you want to use your own module remeber to start with ./
-const names = require('./4-names')
-const sayHi = require('./5-utils')
-const data = require('./6-alternative-flavor')
-
-require('./7-mind-grenade')
-
-// console.log(data)
-
-// sayHi('john')
-// sayHi(names.susan)
-// sayHi(names.peter)
